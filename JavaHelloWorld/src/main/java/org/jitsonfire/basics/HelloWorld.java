c7c842eb676a4de9b447f6d74aa46ebf50573c7a
Jithin Mohanan <jitsonfire@gmail.com>
2020-03-07 20:49:22 +0530

    Added test code

--- JavaHelloWorld/src/main/java/org/jitsonfire/basics/HelloWorld.java
+++ JavaHelloWorld/src/main/java/org/jitsonfire/basics/HelloWorld.java
@@ -2,17 +2,41 @@ package org.jitsonfire.basics;
 
 public class HelloWorld {
 
+	@FunctionalInterface
 	interface Greet {
 
 		void greet();
 	}
 
+	@FunctionalInterface
+	interface Add {
+		int add(int a, int b);
+	}
+
+	@FunctionalInterface
+	interface Square {
+		int square(int a);
+	}
+
 	public static void main(String[] args) {
 
+		// defination of the interfaces
 		Greet g1 = () -> {
 			System.out.println("This is lamda 1");
 		};
 		g1.greet();
+
+		Add a1 = (a, b) -> {
+			return a + b;
+		};
+
+		Square s1 = (int a) -> {
+
+			return a * a;
+		};
+
+		System.out.println(a1.add(1, 2));
+		System.out.println(s1.square(2));
 	}
 
 }
