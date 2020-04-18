74fcb0c9c859aff4cb672a6da3a83e62a8e5688b
JIthin Iyyani <iyyani.jithin@gmail.com>
2020-04-18 11:53:50 +0530

    Added project 1 (How to debug)
    How to debug in java

---
+++ HowToDebug/Calculator/src/main/java/org/desi/coder/App.java
@@ -0,0 +1,21 @@
+package org.desi.coder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		// create a calc object
+		Calculator calculator = new Calculator();
+		// add two numbers
+		System.out.println(calculator.add(3, 4));
+
+		for (int i = 1; i < 10; i++) {
+
+			System.out.println(calculator.add(i, i + 1));
+		}
+
+	}
+}
---
+++ HowToDebug/Calculator/src/main/java/org/desi/coder/Calculator.java
@@ -0,0 +1,10 @@
+package org.desi.coder;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+
+		int sum = a + b;
+		return sum;
+	}
+}
