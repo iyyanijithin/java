d6ad6c8190524cda85445ac03a9db0de1cee67ab
JIthin Iyyani <iyyani.jithin@gmail.com>
2020-04-19 17:08:57 +0530

    Added project for algorithm
    Added buble sort

---
+++ HowToDebug/Algorithm/src/main/java/org/desi/coder/algorithm/App.java
@@ -0,0 +1,16 @@
+package org.desi.coder.algorithm;
+
+import org.desi.coder.algorithm.sorting.BubbleSort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		int[] arr = { 5, 4, 3, 2, 1 };
+		BubbleSort.sort1(arr);
+		int[] arr2 = { 5, 4, 3, 2, 1 };
+		BubbleSort.sort2(arr2);
+	}
+}
---
+++ HowToDebug/Algorithm/src/main/java/org/desi/coder/algorithm/sorting/BubbleSort.java
@@ -0,0 +1,63 @@
+package org.desi.coder.algorithm.sorting;
+
+public class BubbleSort {
+
+	public static void printArray(int[] data) {
+		for (int dataElement : data) {
+			System.out.print(dataElement + " ");
+		}
+		System.out.println("");
+
+	}
+
+	public static void sort1(int[] data) {
+
+		System.out.println("Before sort");
+		printArray(data);
+		int count = 0;
+		for (int i = 0; i < data.length; i++) {
+
+			for (int j = 0; j < data.length - 1; j++) {
+
+				count = count + 1;
+				if (data[j] > data[j + 1]) {
+					int temp = data[j+1];
+					data[j + 1] = data[j];
+					data[j] = temp;					
+				}
+			}
+			System.out.println("Pass "  + (i + 1));
+			printArray(data);
+		}
+		System.out.println("After sort");
+		System.out.println("Total number of time elements were visited " + count);
+		printArray(data);
+
+	}
+	
+	
+	public static void sort2(int[] data) {
+
+		System.out.println("Before sort");
+		printArray(data);
+		int count = 1;
+		for (int i = 0; i < data.length; i++) {
+
+			for (int j = 0; j < data.length - 1 - i; j++) {
+
+				count = count + 1;
+				if (data[j] > data[j + 1]) {
+					int temp = data[j+1];
+					data[j + 1] = data[j];
+					data[j] = temp;
+				}
+			}
+			System.out.println("Pass "  + (i + 1));
+			printArray(data);
+		}
+		System.out.println("After sort");
+		System.out.println("Total number of time elements were visited " + count);
+		printArray(data);
+
+	}
+}
