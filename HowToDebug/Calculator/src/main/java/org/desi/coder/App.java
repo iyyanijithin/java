403e0ff18e72e56088ec3c52114a7b3988c9ed81
JIthin Iyyani <iyyani.jithin@gmail.com>
2020-04-19 16:18:22 +0530

    Updated code
    Updated code

--- HowToDebug/Calculator/src/main/java/org/desi/coder/App.java
+++ HowToDebug/Calculator/src/main/java/org/desi/coder/App.java
@@ -10,11 +10,11 @@ public class App {
 	public static void main(String[] args) {
 
 		// create a calc object
-		Calculator calculator = new Calculator();
+		Calculator calculator = new Calculator(); 
 		// add two numbers
-		System.out.println(calculator.add(3, 4));
+		System.out.println(calculator.add(5, 5));
 
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 100; i++) {
 
 			System.out.println(calculator.add(i, i + 1));
 		}
